47dfe92e67b600c37c3340b93f681e8f743f8763
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-07 13:31:47 +0100

    Initial commit

---
+++ src/main/java/pl/sda/githubdashboard/GithubDashboardApplication.java
@@ -0,0 +1,11 @@
+package pl.sda.githubdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubDashboardApplication.class, args);
+    }
+}
