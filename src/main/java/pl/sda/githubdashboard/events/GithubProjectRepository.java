c2ed3235264bce41b5859591115c379a112806d3
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-09 16:06:42 +0100

    - repository

--- src/main/java/pl/sda/githubdashboard/events/GithubProjectRepository.java
+++ src/main/java/pl/sda/githubdashboard/events/GithubProjectRepository.java
@@ -3,4 +3,5 @@ package pl.sda.githubdashboard.events;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
+    GithubProject findByRepoName(String repoName);
 }
