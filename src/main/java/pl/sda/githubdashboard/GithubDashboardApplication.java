c71c21becc83f13912faeedacf93d1cc02f9fc1c
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-12 00:00:52 +0100

    - GithubProject class continuing...

--- src/main/java/pl/sda/githubdashboard/GithubDashboardApplication.java
+++ src/main/java/pl/sda/githubdashboard/GithubDashboardApplication.java
@@ -2,8 +2,10 @@ package pl.sda.githubdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GithubProperties.class)
 public class GithubDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubDashboardApplication.class, args);
---
+++ src/main/resources/db/migration/V1.0.1__Insert.sql
@@ -0,0 +1,8 @@
+--INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('spring-projects','spring-boot');
+--INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('spring-io','initializr');
+--INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('spring-io','sagan');
+INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('arqarq','GithubDashboard');
+INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('arqarq','vendingMachine');
+INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('arqarq','book-app');
+INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('arqarq','javaktw9-designpatterns');
+INSERT INTO GITHUB_PROJECT(ORG_NAME,REPO_NAME) values ('arqarq','zadania');
